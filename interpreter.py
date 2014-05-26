bda1fe36d8a6b250af8dcfb08c1efd08bb27868b
Alexandre Kandalintsev <spam@messir.net>
2014-05-26 19:01:48 +0200

    interpreter.py: better error reporting in newinfix()

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,7 @@
 from pratt import *
 from ast import *
 
+
 class Int(Leaf):
   def __init__(self, value):
     super().__init__(int(value))
@@ -9,24 +10,38 @@ class Int(Leaf):
     return self
 
   def Add(self, right, frame):
-    assert isinstance(right, Int)
-    return Int(self.value+right.value)
+    return Int(self.value + right.value)
 
   def Sub(self, right, frame):
-    assert isinstance(right, Int)
-    return Int(self.value-right.value)
+    return Int(self.value - right.value)
 
 
-class Str(Leaf): pass
+class Str(Leaf):
+  def eval(self, frame):
+    return self
+  def Add(self, other, frame):
+    return Str(self.value + other.value)
 
 
-def newinfix(sym, prio, methname):
+def newinfix(sym, prio, methname, sametype=True):
   @infix(sym, prio)
   class Infix(Binary):
     def eval(self, frame):
       left = self.left.eval(frame)
       right = self.right.eval(frame)
-      return getattr(left, methname)(right, frame)
+      if sametype:
+        assert type(left) == type(right), \
+          "Left and right operands of ({}) must have same type.\n" \
+          "Got {} and {}.".format(sym, type(left), type(right))
+      try:
+        meth = getattr(left, methname)
+      except AttributeError:
+        raise Exception("{} does not have {} method, \
+                         operation ({}) not supported".format(left, methname, sym))
+      return meth(right, frame)
+  Infix.__name__ = Infix.__qualname__ = methname
+  return Infix
+
 
 newinfix('+', 20, 'Add')
 newinfix('-', 20, 'Sub')
