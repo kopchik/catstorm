f4a0c8bff4fe026a023af48327079a5c0ea006b0
Alexandre Kandalintsev <spam@messir.net>
2014-05-26 22:56:07 +0200

    storm.py: parse expressions passed via commandline

--- storm.py
+++ storm.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 
-from grammar import *
-
-
-from log import logfilter
-# from ast import parse, pretty_fmt
-# from indent import parse as indent_parse
-# from interpreter import run
-from peg import tokenize
-from grammar import EXPR
 from pratt import parse as pratt_parse
+from grammar import EXPR, PROG
+from log import logfilter
+from peg import tokenize
+from frame import Frame
 from sys import exit
 import argparse
+
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -26,7 +22,8 @@ if __name__ == '__main__':
   parser.add_argument('-c', '--check-types', action='store_const', const=True,
                       default=False, help="perform type inference and checking (disabled by default)")
   # parser.add_argument('input', help="path to file")
-  parser.add_argument('-r', '--raw', help="specify raw expression to execute")
+  parser.add_argument('-r', '--raw', help="specify raw expression to execute",
+                      nargs="*")
   parser.add_argument('cmd', nargs="*")
   args = parser.parse_args()
 
@@ -39,18 +36,20 @@ if __name__ == '__main__':
   else:          logfilter.default = False
 
   if args.raw:
-    src = args.raw
-    if args.debug:
-      print("parsing:", src)
-    tokens = tokenize(src)
-    if args.tokens:
-      print(tokens)
-    tokens, r = EXPR.match(tokens)
-    print(tokens)
-    prog = pratt_parse(tokens)
-    print(prog)
-    result = prog.eval({})
-    print(result)
+    with Frame() as frame:
+      for src in args.raw:
+        if args.debug:
+          print("parsing:", src)
+        tokens = tokenize(src)
+        if args.tokens:
+          print(tokens)
+        prog, r = PROG.match(tokens)
+        # print(tokens)
+        # prog = pratt_parse(tokens)
+        # print(prog)
+        # print("PROG", prog)
+        result = prog.eval(frame)
+        print("result:", result)
     exit("DONE")
 
   with open(args.input) as fd:
