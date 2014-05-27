919812924cb9e01366bdfee176a23154c75f8258
Alexandre Kandalintsev <spam@messir.net>
2014-05-27 22:33:17 +0200

    storm.py: clean-ups

--- storm.py
+++ storm.py
@@ -14,22 +14,23 @@ import argparse
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('-t', '--tokens', action='store_const', const=True,
-                      default=False, help="show tokens")
+  # parser.add_argument('-t', '--tokens', action='store_const', const=True,
+  #                     default=False, help="show tokens")
   parser.add_argument('-a', '--ast', action='store_const', const=True,
                       default=False, help="show abstract syntax tree")
   parser.add_argument('-d', '--debug', action='store_const', const=True,
                       default=False, help="show intermediate output")
-  parser.add_argument('-n', '--dry-run', action='store_const', const=True,
-                      default=False, help="do not execute the program")
-  parser.add_argument('-c', '--check-types', action='store_const', const=True,
-                      default=False, help="perform type inference and checking (disabled by default)")
+  # parser.add_argument('-n', '--dry-run', action='store_const', const=True,
+  #                     default=False, help="do not execute the program")
+  # parser.add_argument('-c', '--check-types', action='store_const', const=True,
+  #                     default=False, help="perform type inference and checking (disabled by default)")
   parser.add_argument('-r', '--raw', help="specify raw expression to execute",
                       nargs="*")
   parser.add_argument('input', help="path to file")
   parser.add_argument('cmd', nargs="*")
   args = parser.parse_args()
-  print(args)
+  if args.debug:
+    print("arguments:", args)
 
   logfilter.rules = [
     # ('interpreter.*', False),
@@ -39,7 +40,7 @@ if __name__ == '__main__':
   if args.debug: logfilter.default = True
   else:          logfilter.default = False
 
-  # ACCEPT INPUT FROM COMMAND LINE
+  # INPUT FROM COMMAND LINE
   if args.raw:
     with Frame() as frame:
       for src in args.raw:
@@ -55,11 +56,9 @@ if __name__ == '__main__':
         print("result:", result)
     exit(result)
 
-  # INPUT FROM FILE
-  with open(args.input) as fd:
-    src = fd.read()
 
-  indented = indent_parse(src)
+  # INPUT FROM FILE
+  # stage 1: parse
   def traverse(tree, blk):
     for e in tree:
       if isinstance(e, list):
@@ -68,25 +67,16 @@ if __name__ == '__main__':
         tokens = tokenize(e)
         prog, r = PROG.match(tokens)  # TODO: check r
         blk.append(prog)
+
+  with open(args.input) as fd:
+    src = fd.read()
+  indented = indent_parse(src)
   mainblk = Block()
   traverse(indented, mainblk)
+  if args.ast:
+    print(mainblk)
+
+  # stage 2: execute and return
   with Frame() as frame:
     ret = mainblk.eval(frame)
   exit(ret)
-
-
-  # split source into tokens
-  tokens = tokenize(src)
-  if args.tokens:
-    print(tokens)
-  # parse indentation
-  ast = indent_parse(tokens)
-  # finalize AST generation
-  ast = parse(ast)
-  if args.ast:
-    print(pretty_fmt(ast))
-  # form main() arguments
-  cmd = [args.input]+args.cmd
-  # run the program
-  if not args.dry_run:
-    exit(run(ast, cmd, check_types=args.check_types))
