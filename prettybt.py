6abb1cd54ca8a438a5f77b755770c921556c3b8c
Alexandre Kandalintsev <spam@messir.net>
2014-08-24 16:52:41 +0100

    prettybt.py: improved argument print

--- prettybt.py
+++ prettybt.py
@@ -18,13 +18,13 @@ def prettybt(exctype, exc_val, tb):
   frames = inspect.getinnerframes(tb)
   for frame, file, line, funcname, *other in frames:
     arginfo = inspect.getargvalues(frame)
-    args = tuple(arginfo.args)
+    args = ", ".join(arginfo.args)
     f_locals = frame.f_locals
     if 'self' in f_locals:
       klass = f_locals['self'].__class__.__name__
-      prints("${file}:${line}: ${klass}.${funcname}${args}")
+      prints("${file}:${line}: ${klass}.${funcname}(${args}):")
     else:
-      prints("${file}:${line}: ${funcname}${args}")
+      prints("${file}:${line}: ${funcname}(${args}):")
     tb = tb.tb_next
   prints("${exctype.__name__}: ${exc_val}")
 
